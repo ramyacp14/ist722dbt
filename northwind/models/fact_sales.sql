6f3945bc0dbe09763d73b79f5bef0592676cdc5b
Ramya Chowdary Patchala <rpatchal@g.syr.edu>
2024-03-20 23:24:55 +0000

    commitfie

---
+++ northwind/models/fact_sales.sql
@@ -0,0 +1,25 @@
+with stg_orders as 
+(
+    select
+        orderid,  
+        {{ dbt_utils.generate_surrogate_key(['employeeid']) }} as employeekey, 
+        {{ dbt_utils.generate_surrogate_key(['customerid']) }} as customerkey, 
+        replace(to_date(orderdate)::varchar,'-','')::int as orderdatekey
+    from {{source('northwind','Orders')}}
+),
+stg_order_details as
+(
+    select 
+        orderid,
+        Quantity as quantity, 
+	(Quantity * UnitPrice) as extendedpriceamount,
+	(Quantity * UnitPrice * Discount) as discountamount,
+        (Quantity*UnitPrice*(1-Discount)) as soldamount
+    from {{source('northwind','Order_Details')}}
+)
+
+select  
+    o.*,
+    od.*
+    from stg_orders o
+    join stg_order_details od on o.orderid = od.orderid
